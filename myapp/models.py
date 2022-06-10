87b530940524218d8253188d24c167044c3a2f6d
tina89312 <tina89312@gmail.com>
2022-06-10 17:57:34 +0800

    models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class student(models.Model):
+    cName = models.CharField(max_length=20, null=True)
+    def __str__(self):
+        return self.cName
